720c3238ac8482ed190cb5607e81680702f48809
ElOne1996 <mhuamanh@vallegrande.edu.pe>
2019-03-07 10:01:49 -0500

    MENSAJE DE CONFIRMACION DE REGISTRO

--- SistPromo/src/java/controlador/AlumnoC.java
+++ SistPromo/src/java/controlador/AlumnoC.java
@@ -5,11 +5,16 @@ import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import modelo.AlumnoM;
+import org.primefaces.PrimeFaces;
 
 @Named(value = "alumnoC")
 @SessionScoped
@@ -17,6 +22,9 @@ public class AlumnoC implements Serializable {
 
     AlumnoM alumno = new AlumnoM();
 
+        Calendar Cal = Calendar.getInstance();
+    
+    String fechaActual = Cal.get(Calendar.YEAR) + "/" + (Cal.get(Calendar.MONTH) + 01) + "/" + Cal.get(Calendar.DATE);
     
     public void limpiar(){
         alumno = new AlumnoM();
@@ -40,6 +48,23 @@ public class AlumnoC implements Serializable {
     public List<String> completeTextUbi (String query) throws SQLException, Exception{
         AlumnoD dao = new AlumnoD();
         return dao.queryAutoCompleteUbi(query);
+    }
+    
+    
+    public  void showMessages() throws ParseException {
+        String fechaHoy = Cal.get(Calendar.DATE) + "/" + (Cal.get(Calendar.MONTH) + 01) + "/" + Cal.get(Calendar.YEAR);
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat formatoDelTexto = new SimpleDateFormat("dd/MM/yyyy");
+        String strFecha = fechaHoy;
+        Date fecha = null;
+        fecha = formatoDelTexto.parse(strFecha);
+        SimpleDateFormat formatoDeFecha = new SimpleDateFormat("dd/MM/yyyy");
+        cal.setTime(fecha);
+        cal.add(Calendar.DATE, 5);
+        System.out.println(formatoDeFecha.format(cal.getTime()));
+        
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "¡Bienvenido!", "Usted se ha registrado correctamente  " + "Fecha de Examen  "+formatoDeFecha.format(cal.getTime())+"  Aula: 1");
+        PrimeFaces.current().dialog().showMessageDynamic(message);
     }
    
     
--- SistPromo/web/resources/js/CodigoQr.js
+++
@@ -1,5 +0,0 @@
-var qrcode = new QRCode("qrcode");
-
-$("#text").on("keyup", function() {
-  qrcode.makeCode($(this).val());
-}).keyup().focus();
