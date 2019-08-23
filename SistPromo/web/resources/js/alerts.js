52822960987cebda9ec52e43c54f29659a434480
ElOne1996 <mhuamanh@vallegrande.edu.pe>
2019-08-23 12:17:52 -0500

    validet

---
+++ SistPromo/web/resources/js/alerts.js
@@ -0,0 +1,12 @@
+
+$(document).on("ready", function () {
+    $('#Login-page').css({'height': window.innerHeight + 'px'});
+});
+
+function growlColour() {
+    $(".ui-growl-image-info").parent().parent().addClass("g-info");
+    $(".ui-growl-image-warn").parent().parent().addClass("g-warn");
+    $(".ui-growl-image-error").parent().parent().addClass("g-error");
+    $(".ui-growl-image-fatal").parent().parent().addClass("g-fatal");
+}
+
